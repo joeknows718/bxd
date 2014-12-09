c866df9c26a22bec5caa9d718d30da17ef7fd8a6
Joe Knows <joeknows718@gmail.com>
2014-12-09 16:05:09 -0500

    added new sql db backend prep for port to live env

--- BronxDitial/settings.py
+++ BronxDitial/settings.py
@@ -65,8 +65,13 @@ WSGI_APPLICATION = 'BronxDitial.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'BXD_DB',
+        'HOST': 'localhost',
+        'DATABASE_USER': 'joeknows718'
+        'DATABASE_PASSWORD':'tsab11211'),
     }
 }
 
